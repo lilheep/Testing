f409a1e2e9c3b557d19507e3dab251a2af0d2b29
lilheep <andrey.arkhipov.2006@mail.ru>
2026-02-08 16:07:29 +0300

    pipeline test

--- src/test/java/constants/ApiConstants.java
+++ src/test/java/constants/ApiConstants.java
@@ -6,9 +6,9 @@ public final class ApiConstants {
     @Getter
     private static final String baseUrl = "https://reqres.in/";
     @Getter
-    private static final String token = getToken();
+    private static final String token = getApiToken();
 
-    private static String getToken() {
+    private static String getApiToken() {
         return System.getProperty("apiKey");
     }
 }
